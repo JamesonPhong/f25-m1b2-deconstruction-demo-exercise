67e0e44801c829edb9ae7d0b70656ac1abfddcff
JamesonPhong <000882371@mohawkcollege.ca>
2025-09-16 17:05:51 -0400

    Add checking if Player has enough gold for Bronze Sword.

--- deconstruction-demo-exercise/Program.cs
+++ deconstruction-demo-exercise/Program.cs
@@ -7,3 +7,19 @@ Console.WriteLine("");
 Console.WriteLine("Forgive me for being rude, but how many gold coins do you have?");
 int playerGold = int.Parse(Console.ReadLine());
 Console.WriteLine("");
+
+// Check if Player has enough Gold to trade
+if (playerGold >= 15)
+{
+    Console.WriteLine($"Ah, Sir {playerName}, you are a wealthy one! Please, come in and have a look at this Bronze Sword I have.");
+    Console.WriteLine("It's a fine weapon, perfect for any adventurer, and it costs 15 gold!");
+    Console.WriteLine("");
+
+    int swordPrice = 15;
+    playerGold -= swordPrice;
+    Console.WriteLine($"You have purchased the Bronze Sword for {swordPrice} gold. You now have {playerGold} gold left. Safe travels, Sir {playerName}!");
+}
+else
+{
+    Console.WriteLine($"I'm sorry, Sir {playerName}, but it seems you don't have enough gold to trade with me. Safe travels!");
+}
