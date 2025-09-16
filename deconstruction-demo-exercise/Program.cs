92c1cd7260163ab7ce7c04f39e72899578bf6f38
JamesonPhong <000882371@mohawkcollege.ca>
2025-09-16 17:05:26 -0400

    Add grabbing Player Gold amount.

--- deconstruction-demo-exercise/Program.cs
+++ deconstruction-demo-exercise/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Greetings Traveller! What is your name?");
 string playerName = Console.ReadLine();
 Console.WriteLine("");
+
+// Get Player Gold Amount
+Console.WriteLine("Forgive me for being rude, but how many gold coins do you have?");
+int playerGold = int.Parse(Console.ReadLine());
+Console.WriteLine("");
